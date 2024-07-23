4d0d7ca8fa14a4a5f4946a6a9ccc34e1c970fe8b
eumorin <98094128+eumorin@users.noreply.github.com>
2024-07-23 18:15:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 url = "https://www.avito.ru/user/0b72533487d72ed39f0b45f68712a56a/profile?src=search_seller_info"
 host = "127.0.0.1"
 user = "postgres"
-password = "goga182002"
+password = ""
 db_name = "avito_vehicles"
-mail_password = "goga18202"
+mail_password = ""
 google_code = "vgqqvnrhunberjnx"
 mails = ["ethernetcake@gmail.com", "matveus3@gmail.com", "matveus74@gmail.com"]
